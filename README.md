6cf398479a0f047f552451ae2273f1cda7d1d028
Václav Hodek <vaclav.hodek@lwi.cz>
2020-09-17 22:12:46 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Floating Windows on Android 2: Foreground Service
+
+This repository contains the source code for article [Floating Windows on Android 2: Foreground Service](https://localazy.com/blog/floating-windows-on-android-2-foreground-service).
+
---
+++ app/src/main/java/com/localazy/quicknote/Tools.kt
@@ -0,0 +1,16 @@
+package com.localazy.quicknote
+
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
+fun Context.startFloatingService(command: String = "") {
+    val intent = Intent(this, FloatingService::class.java)
+    if (command.isNotBlank()) intent.putExtra(INTENT_COMMAND, command)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        this.startForegroundService(intent)
+    } else {
+        this.startService(intent)
+    }
+}
+
